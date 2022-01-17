4512c252a12648913eed277077a075ba75587ab9
Ming Daryl <93841730+Daryl1435@users.noreply.github.com>
2022-01-17 17:22:21 +0100

    First commit

---
+++ src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>modal content</p>
+        </div>
+    </div>
+</template>
+
+<style >
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 200px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background: lightgray;
+        width: 100%;
+        height: 100%;
+    }
+</style>
